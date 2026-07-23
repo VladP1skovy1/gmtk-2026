afc98aaaf3b14ffd7684a8644baf83e08ee27510
Yakov Balashov <yakov.balashov@gmail.com>
2026-07-23 20:13:40 +0200

    feat: add rocket slider

--- Assets/Scripts/UI/RocketSlider.cs
+++ Assets/Scripts/UI/RocketSlider.cs
@@ -1,17 +1,29 @@
-using LaunchBad.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LaunchBad.UI
 {
     public class RocketSlider : MonoBehaviour
     {
+        [SerializeField] private RectTransform pointer;
+        private Slider _slider;
+        private Vector2 _anchorX;
+        
+        private void Awake()
+        {
+            _slider = GetComponent<Slider>();
+            _anchorX = new Vector2(pointer.anchorMin.x, pointer.anchorMax.x);
+        }
+
         public void SetValue(float value)
         {
+            _slider.value = value;
         }
 
         public void SetPointer(float value)
         {
-            
+            pointer.anchorMin = new Vector2(_anchorX.x, value);
+            pointer.anchorMax = new Vector2(_anchorX.y, value);
         }
     }
 }
