96c84cbf29864b07c8be117d9ec6ccc9b1c7e7c9
Yakov Balashov <yakov.balashov@gmail.com>
2026-07-23 19:09:26 +0200

    feat: add slider skeleton

---
+++ Assets/Scripts/UI/Slider.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace LaunchBad.UI
+{
+    public class Slider : MonoBehaviour
+    {
+        public void SetValue(float value)
+        {
+            
+        }
+
+        public void SetPointer(float value)
+        {
+            
+        }
+    }
+}
